04b34f10b3682a461f9e509c960493ef231b9c8d
EliasSahlos <esahlos12@gmail.com>
2024-01-05 16:06:54 +0200

    Minor design changes.

--- components/Modal/Photo-Modal/photo-modal.jsx
+++ components/Modal/Photo-Modal/photo-modal.jsx
@@ -53,7 +53,7 @@ function ModalComponent({itemImg, itemDesc, itemTitle, onModalIsClosed}) {
                             </motion.div>
                         )}
                     </AnimatePresence>
-                    <div className="absolute left-1/2 transform -translate-x-1/2 md:bottom-2 md:left-5 2xl:left-24 2xl:bottom-10">
+                    <div className="absolute left-1/2 transform -translate-x-1/2 md:bottom-2 md:left-5 2xl:left-14 2xl:bottom-5">
                         <ModalCloseButton onModalCloseHandler={modalCloseHandler}/>
                         <ModalInfoButton onDescriptionButtonClickHandler={descriptionButtonClickHandler}/>
                     </div>
