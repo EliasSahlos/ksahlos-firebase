88e8b6deeb9a4895007b870cda49069fefe2e7c6
EliasSahlos <esahlos12@gmail.com>
2024-01-05 12:26:25 +0200

    Minor header change

--- components/Layout/Header/Header.jsx
+++ components/Layout/Header/Header.jsx
@@ -20,8 +20,8 @@ function Header() {
     return (
         <div className="flex justify-between items-center bg-white h-[32px] relative">
             <div className="flex-1">
-                <div className="flex justify-center items-center scale-90 hover:scale-100 ease-in duration-300">
-                        <Link href={"/"}>
+                <div className="flex justify-center items-center scale-90 hover:scale-100 ease-in duration-300 cursor-pointer">
+                        {/* <Link href={"/"}>
                             <Image 
                             src={'https://firebasestorage.googleapis.com/v0/b/photography-site-ksahlos-84194.appspot.com/o/Others%2Flogo.png?alt=media&token=661d3314-49d1-4d54-a0fb-e084a54fe783'} 
                             alt="logo"
@@ -29,7 +29,8 @@ function Header() {
                             height={350}
                             loading="lazy"
                             />
-                        </Link>
+                        </Link> */}
+                        Logo Image
                 </div>
             </div>
             <div className="absolute right-0 ml=[100px] md:hover:scale-110 z-30 ease-in duration-200" onClick={hamburgerClickHandler}>
