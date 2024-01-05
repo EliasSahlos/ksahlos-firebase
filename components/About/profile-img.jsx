92ed432ab9f3993697c9cc06b936870afb897f5f
EliasSahlos <esahlos12@gmail.com>
2024-01-05 15:24:26 +0200

    Minor design changes.

--- components/About/profile-img.jsx
+++ components/About/profile-img.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function ProfileImage({width,height}) {
     const imageSrc =
-        "https://firebasestorage.googleapis.com/v0/b/photography-site-ksahlos-84194.appspot.com/o/Others%2Fksahlos-profile-img.jpg?alt=media&token=3808a140-06fe-4d4d-b4dc-32d8f084b61e";
+        "https://firebasestorage.googleapis.com/v0/b/photography-site-ksahlos-84194.appspot.com/o/Others%2Fabout-picture.jpg?alt=media&token=395fc7c7-919d-40d0-bb25-5aab0e2decca";
 
     return (
         <Image src={imageSrc} alt="profile-pic" width={width} height={height} className="rounded shadow-md" loading="eager"/>
